56268a3d2e7e79d22c8c4a7d4dd2443edb972d3c
ruurd <ruurdbijlsma@gmail.com>
2017-05-10 16:29:22 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+    loadImages();
+}
+
+async function loadImages(maxImages = Infinity) {
+    const response = await fetch('images.json');
+    let images = await response.json();
+    images = images.slice(images.length - maxImages);
+    console.log(images);
+    let gallery = document.getElementsByClassName('gallery')[0];
+    let html = '';
+    for (let image of images) {
+        let color = generateRandomColor({ r: 255, g: 255, b: 255 });
+        let imageUrl = image.url.split('?dl=0')[0];
+
+        if (image.type === "video") {
+            html += `
+            <div class='media-container'>
+                <video src="${imageUrl}?dl=1" controls></video>
+            </div>`;
+        } else {
+            html += `
+            <div class="media-container" style="background-color: rgba(${color.r}, ${color.g}, ${color.b}, 1)">
+                <a href="${imageUrl}?dl=0">
+                    <div class="foto" style="background-image:url(${imageUrl}?dl=1)"></div>
+                </a>
+            </div>`;
+        }
+    }
+    gallery.innerHTML = html;
+}
+
+function generateRandomColor(mix) {
+    let red = Math.random() * 256;
+    let green = Math.random() * 256;
+    let blue = Math.random() * 256;
+
+    // mix the color
+    if (mix != null) {
+        red = (red + mix.r) / 2;
+        green = (green + mix.r) / 2;
+        blue = (blue + mix.r) / 2;
+    }
+
+    return {
+        r: Math.floor(red),
+        g: Math.floor(green),
+        b: Math.floor(blue)
+    };
+}
